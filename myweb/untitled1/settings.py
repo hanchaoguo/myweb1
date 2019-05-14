5dfae853440dc397677b759a92fa30468bf1231d
liukai <651002081@qq.com>
2019-05-14 10:03:43 +0800

    修改添加资产，规划前端template，统一放到cmdb目录下

--- myweb/untitled1/settings.py
+++ myweb/untitled1/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'myweb',
         'USER': 'root',
-        'PASSWORD': '!QAZ2wsx',
+        'PASSWORD': '122356',
         'HOST': 'localhost',
         'PORT': '3306',
     }
